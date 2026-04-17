7617520aa39dcccfda347894c2ba51a43eaa293a
Calmax Omondi <calmax.omondi@student.moringaschool.com>
2026-04-17 10:16:37 +0300

    an update to the repo

---
+++ index.js
@@ -0,0 +1,4 @@
+const readline = require("readline-sync");
+
+
+let score = 0
---
+++ questions.js
@@ -0,0 +1,12 @@
+const questions = [ {
+    question: "Which team won the 2018 Kenya Premier League competition? ",
+    options: ["A. Gor Mahia", "B. AFC Leopards", "C. Tusker", "D. Bandari"],
+    answer: "A"
+},
+{
+    question: "Who scored the first top flight goal for Murang'a Seal FC? ",
+    options: ["A. Joe Waithera", "B. Batts Awita", "C. Dennis Omino", "D. Yusuf Ali"],
+    answer: "B"
+}
+]
+module.exports = questions
